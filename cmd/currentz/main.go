ec7deb8ee3ee9ea310ed04a17d7cc3c0bf251b40
jdelles <james.delles@gmail.com>
2025-08-30 18:08:39 -0400

    adding main.go

---
+++ cmd/currentz/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/jdelles/currentz/internal/app"
+	"github.com/jdelles/currentz/internal/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	financeApp, err := app.NewFinanceApp(cfg)
+	if err != nil {
+		log.Fatalf("Failed to initialize app: %v", err)
+	}
+	defer financeApp.Close()
+
+	if err := financeApp.Run(); err != nil {
+		log.Fatalf("Application error: %v", err)
+		os.Exit(1)
+	}
+}
